8cf8560d4f0ecdb6654028a5a0cc81bfbb3e7284
haikalzhir <haikalzhir@gmail.com>
2025-06-25 00:37:04 +0700

    Update Hewan.java

--- Hewan.java
+++ Hewan.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum17042025;
 
-/**
- *
- * @author nofri
- */
+package praktikum17042025;
 public class Hewan {
     protected String nama;
     
