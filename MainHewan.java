f632eb14e2b4fb5385568a39c0c852ac3ebae333
haikalzhir <haikalzhir@gmail.com>
2025-06-25 00:38:09 +0700

    Update MainHewan.java

--- MainHewan.java
+++ MainHewan.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktikum17042025;
 
-/**
- *
- * @author nofri
- */
+
 public class MainHewan {
     
     public static void main (String[]args){
